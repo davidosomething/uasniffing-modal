43db03437fc66e278a2f73c18c7dd6d8eb70dbb9
David O'Trakoun <me@davidosomething.com>
2012-03-12 15:44:48 -0400

    modal has ab option to show only to 50% of users

--- modal.js
+++ modal.js
@@ -1,9 +1,30 @@
-var mobileSurveyModal = (function() {
+/**
+ * Survey Modal JavaScript
+ *
+ * Depends on jQuery 1.7.1+
+ * Replace $.on with $.delegate if need to backport
+ *
+ * Usage:
+ * Call with surveyModal.init();
+ *
+ * Parameters
+ * surveyModal.init({
+ *  debug: true,
+ *  ab: true
+ * });
+ */
+var surveyModal = (function () {
   var m = this;
 
+  this.options = {
+    debug: false,
+    ab: false
+  };
+
   // detect mobile browsers, case insensitive
-  this.isMobile = function() {
+  this.isMobile = function () {
     if (navigator.userAgent.match(/Android/i)
+     || navigator.userAgent.match(/Kindle/i)
      || navigator.userAgent.match(/webOS/i)
      || navigator.userAgent.match(/iPhone|iPod|iPad/i)
      || navigator.userAgent.match(/Blackberry|Rim|WebOS/i)
@@ -14,15 +35,17 @@ var mobileSurveyModal = (function() {
     return false;
   };
 
-  this.isMobileSurveyDone = function() {
+  this.isMobileSurveyDone = function () {
     var cookieValue = document.cookie.match('(^|;) ?isMobileSurveyDone=([^;]*)(;|$)');
     return cookieValue && (cookieValue[2] == 1) ? true : false;
-  }
+  };
 
   // read survey cookie, show survey
-  this.show = function() {
-    if ($('#mobile_survey_modal').length) return;
-    if (window.mobileSurveyDebug) m.debug();
+  this.show = function () {
+    if ($('#mobile_survey_modal').length) {
+      return;
+    }
+    m.debug();
 
     var modalHtml = '';
     modalHtml += '<div id="mobile_survey_modal">';
@@ -33,27 +56,54 @@ var mobileSurveyModal = (function() {
     $(modalHtml).appendTo('body');
   };
 
-  this.hide = function(e) {
+  this.hide = function (e) {
     $('#mobile_survey_modal').remove();
   };
 
-  this.done = function(e) {
+  this.done = function (e) {
     document.cookie = 'isMobileSurveyDone=1'; // unexpiring cookie
     m.hide();
   };
 
-  this.undo = function(e) {
+  this.undo = function (e) {
     document.cookie = 'isMobileSurveyDone=0'; // unexpiring cookie
   };
 
-  this.debug = function() {
+  this.debug = function () {
+    if (!m.options.debug) {
+      return;
+    }
     console.log('isMobile? ' + m.isMobile());
     console.log('isMobileSurveyDone? ' + m.isMobileSurveyDone());
   };
 
-  this.init = function() {
+  /**
+   * @param options object
+   *  debug: bool   - debug to console?
+   *  ab:    bool   - only open for 50% of people?
+   */
+  this.init = function () {
+    if (arguments.length) {
+      m.options = arguments[0];
+    }
+    if (m.options.debug) {
+      console.log(m.options);
+    }
     $('body').on('click', '#mobile_survey_modal-yes, #mobile_survey_modal-no', m.done);
-    if (window.mobileSurveyDebug || (!m.isMobileSurveyDone() && m.isMobile())) m.show();
+    var isInSampleGroup = true;
+    if (m.options.ab) {
+      isInSampleGroup = Math.floor(Math.random() * 2);
+    }
+    if (isInSampleGroup) {
+      if (m.options.debug || (!m.isMobileSurveyDone() && m.isMobile())) {
+        m.show();
+      }
+    }
+    else {
+      if (m.options.debug) {
+        console.log('Not in sample group');
+      }
+    }
   }
 
   return this;
